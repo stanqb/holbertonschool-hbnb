3c27a367d316f7209edf0681ff4886489435ae69
stan-holberton <9857@holbertonstudents.com>
2025-02-28 14:19:32 +0100

    Update the services/facade.py file to include business logic related to amenities.

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,34 +1,34 @@
-import uuid
+from app.models.amenity import Amenity
+from app.persistence.repository import Repository
 
 
 class HBnBFacade:
     def __init__(self):
-        self.amenities = []
+        self.repository = Repository()
 
     def create_amenity(self, amenity_data):
-        """ Creates a new amenity and returns the created object """
-        new_amenity = {
-            'id': str(uuid.uuid4()),  # Generates a unique ID
-            'name': amenity_data['name']
-        }
-        self.amenities.append(new_amenity)
-        return new_amenity
+        """Creates a new amenity and saves it to the database."""
+        if 'name' not in amenity_data or not amenity_data['name']:
+            return None, "Amenity name required"
+        new_amenity = Amenity(name=amenity_data['name'])
+        self.repository.save(new_amenity)
+        return new_amenity, None
 
     def get_amenity(self, amenity_id):
-        """ Retrieves an amenity by its ID """
-        for amenity in self.amenities:
-            if amenity['id'] == amenity_id:
-                return amenity
-        return None  # Returns None if the amenity is not found
+        """Retrieves an amenity by its ID."""
+        return self.repository.get_by_id(Amenity, amenity_id)
 
     def get_all_amenities(self):
-        """ Returns the complete list of amenities """
-        return self.amenities
+        """Retrieves all available amenities."""
+        return self.repository.get_all(Amenity)
 
     def update_amenity(self, amenity_id, amenity_data):
-        """ Updates an amenity with the provided data """
-        for amenity in self.amenities:
-            if amenity['id'] == amenity_id:
-                amenity['name'] = amenity_data['name']
-                return amenity
-        return None  # Returns None if the amenity is not found
+        """Updates the information of an existing amenity."""
+        amenity = self.repository.get_by_id(Amenity, amenity_id)
+        if not amenity:
+            return None, "Amenity not found"
+        if 'name' in amenity_data and amenity_data['name']:
+            amenity.name = amenity_data['name']
+            self.repository.save(amenity)
+            return amenity, None
+        return None, "Invalid data"
